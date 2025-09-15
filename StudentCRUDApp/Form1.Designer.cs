11c22c70304ec7e9866402eb3ba24dcb47ac8929
kianixx <kianacirilo23@gmail.com>
2025-09-15 00:47:03 -0800

    first commit

---
+++ StudentCRUDApp/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace StudentCRUDApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_fname = new System.Windows.Forms.Label();
+            this.lb_lname = new System.Windows.Forms.Label();
+            this.lb_age = new System.Windows.Forms.Label();
+            this.lb_course = new System.Windows.Forms.Label();
+            this.tb_fname = new System.Windows.Forms.TextBox();
+            this.tb_lname = new System.Windows.Forms.TextBox();
+            this.tb_age = new System.Windows.Forms.TextBox();
+            this.tb_course = new System.Windows.Forms.TextBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_edit = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.dataTable = new System.Windows.Forms.DataGridView();
+            this.tb_studentID = new System.Windows.Forms.TextBox();
+            this.lb_id = new System.Windows.Forms.Label();
+            this.btn_refresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb_fname
+            // 
+            this.lb_fname.AutoSize = true;
+            this.lb_fname.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_fname.Location = new System.Drawing.Point(35, 37);
+            this.lb_fname.Name = "lb_fname";
+            this.lb_fname.Size = new System.Drawing.Size(97, 24);
+            this.lb_fname.TabIndex = 0;
+            this.lb_fname.Text = "First Name:";
+            // 
+            // lb_lname
+            // 
+            this.lb_lname.AutoSize = true;
+            this.lb_lname.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_lname.Location = new System.Drawing.Point(35, 70);
+            this.lb_lname.Name = "lb_lname";
+            this.lb_lname.Size = new System.Drawing.Size(95, 24);
+            this.lb_lname.TabIndex = 1;
+            this.lb_lname.Text = "Last Name:";
+            // 
+            // lb_age
+            // 
+            this.lb_age.AutoSize = true;
+            this.lb_age.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_age.Location = new System.Drawing.Point(35, 105);
+            this.lb_age.Name = "lb_age";
+            this.lb_age.Size = new System.Drawing.Size(43, 24);
+            this.lb_age.TabIndex = 2;
+            this.lb_age.Text = "Age:";
+            // 
+            // lb_course
+            // 
+            this.lb_course.AutoSize = true;
+            this.lb_course.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_course.Location = new System.Drawing.Point(35, 140);
+            this.lb_course.Name = "lb_course";
+            this.lb_course.Size = new System.Drawing.Size(67, 24);
+            this.lb_course.TabIndex = 3;
+            this.lb_course.Text = "Course:";
+            // 
+            // tb_fname
+            // 
+            this.tb_fname.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.tb_fname.Font = new System.Drawing.Font("Bahnschrift Light SemiCondensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_fname.Location = new System.Drawing.Point(152, 34);
+            this.tb_fname.Name = "tb_fname";
+            this.tb_fname.Size = new System.Drawing.Size(225, 29);
+            this.tb_fname.TabIndex = 4;
+            // 
+            // tb_lname
+            // 
+            this.tb_lname.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.tb_lname.Font = new System.Drawing.Font("Bahnschrift Light SemiCondensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_lname.Location = new System.Drawing.Point(152, 69);
+            this.tb_lname.Name = "tb_lname";
+            this.tb_lname.Size = new System.Drawing.Size(225, 29);
+            this.tb_lname.TabIndex = 5;
+            // 
+            // tb_age
+            // 
+            this.tb_age.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.tb_age.Font = new System.Drawing.Font("Bahnschrift Light SemiCondensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_age.Location = new System.Drawing.Point(152, 104);
+            this.tb_age.Name = "tb_age";
+            this.tb_age.Size = new System.Drawing.Size(225, 29);
+            this.tb_age.TabIndex = 6;
+            // 
+            // tb_course
+            // 
+            this.tb_course.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.tb_course.Font = new System.Drawing.Font("Bahnschrift Light SemiCondensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_course.Location = new System.Drawing.Point(152, 139);
+            this.tb_course.Name = "tb_course";
+            this.tb_course.Size = new System.Drawing.Size(225, 29);
+            this.tb_course.TabIndex = 7;
+            // 
+            // btn_add
+            // 
+            this.btn_add.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btn_add.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.Location = new System.Drawing.Point(152, 186);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(115, 45);
+            this.btn_add.TabIndex = 8;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = false;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btn_edit.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_edit.Location = new System.Drawing.Point(403, 96);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(115, 45);
+            this.btn_edit.TabIndex = 9;
+            this.btn_edit.Text = "Edit";
+            this.btn_edit.UseVisualStyleBackColor = false;
+            this.btn_edit.Click += new System.EventHandler(this.btn_edit_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.BackColor = System.Drawing.Color.Crimson;
+            this.btn_delete.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_delete.Location = new System.Drawing.Point(528, 96);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(115, 45);
+            this.btn_delete.TabIndex = 10;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = false;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btn_load
+            // 
+            this.btn_load.BackColor = System.Drawing.Color.Khaki;
+            this.btn_load.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_load.Location = new System.Drawing.Point(649, 96);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(115, 45);
+            this.btn_load.TabIndex = 11;
+            this.btn_load.Text = "Load";
+            this.btn_load.UseVisualStyleBackColor = false;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // dataTable
+            // 
+            this.dataTable.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataTable.Location = new System.Drawing.Point(103, 258);
+            this.dataTable.Name = "dataTable";
+            this.dataTable.RowHeadersWidth = 51;
+            this.dataTable.RowTemplate.Height = 24;
+            this.dataTable.Size = new System.Drawing.Size(429, 150);
+            this.dataTable.TabIndex = 12;
+            // 
+            // tb_studentID
+            // 
+            this.tb_studentID.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.tb_studentID.Font = new System.Drawing.Font("Bahnschrift Light SemiCondensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_studentID.Location = new System.Drawing.Point(609, 49);
+            this.tb_studentID.Name = "tb_studentID";
+            this.tb_studentID.Size = new System.Drawing.Size(45, 29);
+            this.tb_studentID.TabIndex = 14;
+            // 
+            // lb_id
+            // 
+            this.lb_id.AutoSize = true;
+            this.lb_id.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_id.Location = new System.Drawing.Point(509, 50);
+            this.lb_id.Name = "lb_id";
+            this.lb_id.Size = new System.Drawing.Size(94, 24);
+            this.lb_id.TabIndex = 13;
+            this.lb_id.Text = "Student ID:";
+            // 
+            // btn_refresh
+            // 
+            this.btn_refresh.BackColor = System.Drawing.Color.Turquoise;
+            this.btn_refresh.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_refresh.Location = new System.Drawing.Point(568, 310);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new System.Drawing.Size(115, 45);
+            this.btn_refresh.TabIndex = 15;
+            this.btn_refresh.Text = "Refresh";
+            this.btn_refresh.UseVisualStyleBackColor = false;
+            this.btn_refresh.Click += new System.EventHandler(this.btn_refresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_refresh);
+            this.Controls.Add(this.tb_studentID);
+            this.Controls.Add(this.lb_id);
+            this.Controls.Add(this.dataTable);
+            this.Controls.Add(this.btn_load);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_edit);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.tb_course);
+            this.Controls.Add(this.tb_age);
+            this.Controls.Add(this.tb_lname);
+            this.Controls.Add(this.tb_fname);
+            this.Controls.Add(this.lb_course);
+            this.Controls.Add(this.lb_age);
+            this.Controls.Add(this.lb_lname);
+            this.Controls.Add(this.lb_fname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_fname;
+        private System.Windows.Forms.Label lb_lname;
+        private System.Windows.Forms.Label lb_age;
+        private System.Windows.Forms.Label lb_course;
+        private System.Windows.Forms.TextBox tb_fname;
+        private System.Windows.Forms.TextBox tb_lname;
+        private System.Windows.Forms.TextBox tb_age;
+        private System.Windows.Forms.TextBox tb_course;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_edit;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.DataGridView dataTable;
+        private System.Windows.Forms.TextBox tb_studentID;
+        private System.Windows.Forms.Label lb_id;
+        private System.Windows.Forms.Button btn_refresh;
+    }
+}
+
---
+++ StudentCRUDApp/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace StudentCRUDApp
+{
+    public partial class Form1: Form
+    {
+        private string connectionString = "Server=MSI\\SQLEXPRESS;Initial Catalog=StudentDB;Integrated Security=True;";
+        public Form1()
+        {
+            InitializeComponent();
+            load_data();
+        }
+
+        private void load_data()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Students", conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dataTable.DataSource = dt;
+            }
+        }
+
+        //ADD
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            string fname = tb_fname.Text;
+            string lname = tb_lname.Text;
+            string age = tb_age.Text;
+            string course = tb_course.Text;
+            string studentID = tb_studentID.Text;
+
+            if (fname == "" || lname == "" || age == "" || course == "")
+            {
+                MessageBox.Show("Incomplete values. Fill all inputs.");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                //MessageBox.Show("Connected!");
+                SqlCommand cmd = new SqlCommand("INSERT INTO Students(FirstName, LastName, Age, Course) VALUES (@FirstName, @LastName, @Age, @Course)", conn);
+                cmd.Parameters.AddWithValue("@FirstName", tb_fname.Text);
+                cmd.Parameters.AddWithValue("@LastName", tb_lname.Text);
+                cmd.Parameters.AddWithValue("@Age", tb_age.Text);
+                cmd.Parameters.AddWithValue("@Course", tb_course.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Student added!");
+                
+            }
+            load_data();
+            clear_tb();
+        }
+
+        //EDIT
+        private void btn_edit_Click(object sender, EventArgs e)
+        {
+            string fname = tb_fname.Text;
+            string lname = tb_lname.Text;
+            string age = tb_age.Text;
+            string course = tb_course.Text;
+            string studentID = tb_studentID.Text;
+
+            if (fname == "" || lname == "" || age == "" || course == "" || studentID == "")
+            {
+                MessageBox.Show("Incomplete values. Fill all inputs.");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "UPDATE Students SET FirstName = @FirstName, LastName = @LastName, Age = @Age, Course = @Course WHERE StudentID = @StudentID";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@FirstName", fname);
+                cmd.Parameters.AddWithValue("@LastName", lname);
+                cmd.Parameters.AddWithValue("@Age", age);
+                cmd.Parameters.AddWithValue("@Course", course);
+                cmd.Parameters.AddWithValue("@StudentID", studentID);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Student updated!");
+            }
+            load_data();
+            clear_tb();
+        }
+
+        //DELETE
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            string studentID = tb_studentID.Text;
+
+            if(studentID == "")
+            {
+                MessageBox.Show("Enter Student ID! to delete");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "DELETE FROM Students WHERE StudentID = @StudentID";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@StudentID", studentID);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Student Deleted!");
+            }
+            load_data();
+            clear_tb();
+        }
+
+        //LOAD
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+            string studentID = tb_studentID.Text;
+
+            if(studentID == "")
+            {
+                MessageBox.Show("Enter student ID to load");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT * FROM Students WHERE StudentID = @StudentID";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                adapter.SelectCommand.Parameters.AddWithValue("@StudentID", studentID);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                dataTable.DataSource = dt;
+
+                if (dt.Rows.Count == 0)
+                {
+                    MessageBox.Show("No student found with that ID.");
+                    return;
+                }
+
+                //kuhaon ang data from database nya ibutang sa texboxes
+                tb_fname.Text = dt.Rows[0]["FirstName"].ToString();
+                tb_lname.Text = dt.Rows[0]["LastName"].ToString();
+                tb_age.Text = dt.Rows[0]["Age"].ToString();
+                tb_course.Text = dt.Rows[0]["Course"].ToString();
+            }
+
+        }
+
+        private void btn_refresh_Click(object sender, EventArgs e)
+        {
+            load_data();
+            clear_tb();
+        }
+
+        private void clear_tb()
+        {
+            tb_fname.Text = "";
+            tb_lname.Text = "";
+            tb_age.Text = "";
+            tb_course.Text = "";
+            tb_studentID.Text = "";
+        }
+    }
+}
